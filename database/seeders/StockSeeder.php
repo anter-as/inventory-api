8217ef49122889b0a52cb677d77d83787a1eb1e9
Ahmed Anter <a.anter@rawafedtech.net>
2025-07-21 03:00:47 +0300

    Add dummy data.

---
+++ database/seeders/StockSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\InventoryItem;
+use App\Models\Stock;
+use App\Models\Warehouse;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StockSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $warehouses = Warehouse::all();
+        $items = InventoryItem::all();
+
+        foreach ($warehouses as $warehouse) {
+            foreach ($items->random(5) as $item) {
+                Stock::create([
+                    'warehouse_id' => $warehouse->id,
+                    'inventory_item_id' => $item->id,
+                    'quantity' => rand(1, 100),
+                ]);
+            }
+        }
+    }
+}
